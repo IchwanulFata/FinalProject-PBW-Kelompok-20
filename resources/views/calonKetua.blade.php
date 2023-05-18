34157147d198cae4e4dcfd4014743ec9f11b54b9
Ardiansyah <ardian21@mhs.unsyiah.ac.id>
2023-05-18 14:11:00 +0700

    PemilihanKetua.html

--- resources/views/calonKetua.blade.php
+++ resources/views/calonKetua.blade.php
@@ -12,7 +12,7 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,300;0,400;0,600;0,700;1,700&display=swap"
         rel="stylesheet" />
-    <!-- sjhdjhsj -->
+
     <script src="https://unpkg.com/feather-icons"></script>
 
     <link rel="stylesheet" href="{{url('css/calonKetua.css')}}" />
